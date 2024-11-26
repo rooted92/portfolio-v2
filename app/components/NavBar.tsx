67e2375cadfc202553182c9b750bc5e183585846
Pedro <petecast76@gmail.com>
2024-11-26 10:41:50 -0800

    feat: created navbar, fixed copy in main heading

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import Link from 'next/link'
+import { link } from 'fs'
+
+interface NavBarLink {
+  href: string,
+  icon: string,
+  iconClasses?: string,
+  linkClasses?: string,
+}
 
 const NavBar = () => {
+
+  const links: NavBarLink[] = [
+    {
+      href: '/',
+      icon: 'icon-[tdesign--circle]',
+    },
+    {
+      href: '/',
+      icon: 'icon-[proicons--github]',
+    },
+    {
+      href: '/',
+      icon: 'icon-[line-md--linkedin]',
+    },
+    {
+      href: '/',
+      icon: 'icon-[proicons--pdf]',
+    },
+  ]
+
   return (
-    <div>NavBar</div>
+    <header>
+      <nav className='flex justify-center items-center gap-6 p-8'>
+        {
+          links.map((link, index) => (
+            <Link key={index} href={link.href} className={`text-2xl hover:text-hookersGreen hover:-translate-y-1 ${link.linkClasses} transition-all ease-in`}><i className={`${link.icon} ${link.iconClasses}`} role="img" aria-hidden="true" /></Link>
+          ))
+        }
+      </nav>
+    </header>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -15,17 +15,17 @@ export default function Home() {
       {/* First Row */}
       <div className="col-span-4 flex flex-around gap-4 border border-honeydew p-8">
         <div className="flex items-center justify-center gap-4">
-          <div className="flex flex-col">
-            <Link href={'/'} className="hover:text-hookersGreen hover:-translate-x-1 transition-all ease-in"><i className="icon-[proicons--github] text-3xl" role="img" aria-hidden="true" /></Link>
-            <Link href={'/'} className="hover:text-hookersGreen hover:-translate-x-1 transition-all ease-in"><i className="icon-[line-md--linkedin] text-3xl" role="img" aria-hidden="true" /></Link>
-            <Link href={'/'} className="hover:text-hookersGreen hover:-translate-x-1 transition-all ease-in"><i className="icon-[proicons--pdf] text-3xl" role="img" aria-hidden="true" /></Link>
-          </div>
-
           <div className="flex items-center justify-center gap-4">
             <Image src={"/pedro.jpg"} alt="Photo of Pedro Castaneda" width={2000} height={2000} className="h-40 w-40 object-cover object-top rounded-full" />
             <div className="flex flex-col">
               <h1 className={`${righteous.className} font-bold tracking-wider text-5xl`}>Pedro Cataneda</h1>
-              <p className="text-lg bg-gradient-to-r from-citrine to-persimmon bg-transparent bg-clip-text text-transparent font-semibold">Software Developer / Former Truck Driver</p>
+              <p className="text-lg bg-gradient-to-r from-citrine to-persimmon bg-transparent bg-clip-text text-transparent font-semibold">Full Stack Developer / Freelancer</p>
+              <div className="flex gap-4">
+                <p>stack</p>
+                <p>icons</p>
+                <p>go</p>
+                <p>here</p>
+              </div>
             </div>
           </div>
 
