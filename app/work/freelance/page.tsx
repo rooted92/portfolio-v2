7bd7f7e47b2dd9333faacbd3ba54ffb7c40582b1
Pedro <petecast76@gmail.com>
2024-12-01 14:12:57 -0800

    fix: imported carousel and link to freelance page

--- app/work/freelance/page.tsx
+++ app/work/freelance/page.tsx
@@ -1,17 +1,19 @@
 import HeadingOne from '@/app/ui/HeadingOne'
 import React from 'react'
+import Image from 'next/image'
+import Carousel from '@/app/components/Carousel'
 
 const page = () => {
   return (
     <section>
-        <HeadingOne text='Freelance' />
-        <p>I’ve developed responsive and intuitive websites for  using Next.js, TypeScript, and Tailwind CSS, designing layouts directly through coding and drawing inspiration from modern web design trends.</p>
-        <div className="flex flex-col items-center mt-6">
+      <HeadingOne text='Freelance' />
+      <p>I’ve developed responsive and intuitive websites for  using Next.js, TypeScript, and Tailwind CSS, designing layouts directly through coding and drawing inspiration from modern web design trends.</p>
+      <div className="flex flex-col items-center mt-6">
         <Image src='/.png' alt='GTR logo' width={2000} height={2000} className='h-20 w-auto mb-4' />
         <Carousel items={['/neyers-screenshot-one.png', '/neyers-screenshot-two.png']} />
       </div>
 
-<div className="flex flex-col items-center mt-6">
+      <div className="flex flex-col items-center mt-6">
         <Image src='/.png' alt='Adelman auto logo' width={2000} height={2000} className='h-20 w-auto mb-4' />
         <Carousel items={['/neyers-screenshot-one.png', '/neyers-screenshot-two.png']} />
       </div>
