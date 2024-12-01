a2975680ac23000f092c4468da17d07b7ae7ed94
Pedro Castaneda <90745973+rooted92@users.noreply.github.com>
2024-12-01 14:04:29 -0800

    feat: added carousels to freelance page

--- app/work/freelance/page.tsx
+++ app/work/freelance/page.tsx
@@ -5,7 +5,16 @@ const page = () => {
   return (
     <section>
         <HeadingOne text='Freelance' />
-        <p>I’ve developed responsive and intuitive websites for mechanics using Next.js, TypeScript, and Tailwind CSS, designing layouts directly through coding and drawing inspiration from modern web design trends.</p>
+        <p>I’ve developed responsive and intuitive websites for  using Next.js, TypeScript, and Tailwind CSS, designing layouts directly through coding and drawing inspiration from modern web design trends.</p>
+        <div className="flex flex-col items-center mt-6">
+        <Image src='/.png' alt='GTR logo' width={2000} height={2000} className='h-20 w-auto mb-4' />
+        <Carousel items={['/neyers-screenshot-one.png', '/neyers-screenshot-two.png']} />
+      </div>
+
+<div className="flex flex-col items-center mt-6">
+        <Image src='/.png' alt='Adelman auto logo' width={2000} height={2000} className='h-20 w-auto mb-4' />
+        <Carousel items={['/neyers-screenshot-one.png', '/neyers-screenshot-two.png']} />
+      </div>
     </section>
   )
 }
