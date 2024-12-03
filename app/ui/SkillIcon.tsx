cb6b497eb78936485f5357d133b1202b4c3bf449
Pedro <petecast76@gmail.com>
2024-12-03 09:15:54 -0800

    reduced scaled on hover

--- app/ui/SkillIcon.tsx
+++ app/ui/SkillIcon.tsx
@@ -16,7 +16,7 @@ interface SkillIconProps {
 const SkillIcon = ({ icon }: SkillIconProps) => {
     return (
         <Link href={icon.url} title={icon.name} target='_blank'>
-            <i className={`${icon.iconName} ${icon.iconClasses} text-hookersGreen hover:text-persimmon dark:hover:text-honeydew hover:scale-110 hover:-translate-y-2 text-2xl transition-all ease-linear`} role="img" aria-hidden="true" />
+            <i className={`${icon.iconName} ${icon.iconClasses} text-hookersGreen hover:text-persimmon dark:hover:text-honeydew hover:scale-110 hover:-translate-y-1 text-2xl transition-all ease-linear`} role="img" aria-hidden="true" />
         </Link>
     )
 }
