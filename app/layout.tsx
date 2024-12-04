77d1b115cc0eaeefc074c5fd75f2d088e24adb91
Pedro Castaneda <petecast76@gmail.com>
2024-12-03 23:32:04 -0800

    fix: removed next/head from layout, used metadata instead

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Ubuntu_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import Head from "next/head";
 
 const ubuntuMono = Ubuntu_Mono({
   subsets: ["latin"],
@@ -13,6 +12,9 @@ const ubuntuMono = Ubuntu_Mono({
 export const metadata: Metadata = {
   title: "Pedro C. | Developer",
   description: "Pedro Castaneda is a software developer and former truck driver. He is a full-stack developer with a focus on front-end development and has professional experience in WordPress, PHP, and JavaScript. He interned at Trinchero Family Estates and is a graduate of the 2023 CodeStack Academy program in Stockton, California.",
+  icons: {
+    icon: "/favicon.ico",
+  }
 };
 
 export default function RootLayout({
@@ -22,9 +24,6 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-      </Head>
       <html lang="en">
         <body
           className={`${ubuntuMono.className} antialiased bg-honeydew text-licorice dark:bg-licorice dark:text-honeydew flex flex-col min-h-screen`}
